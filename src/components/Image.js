966e9901856b553ae6ff89931fb43b06c5c31257
DiegoMutre <tysonmutre10@gmail.com>
2021-02-01 07:10:34 -0500

    feat: Finishi Image component.

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,7 +2,25 @@ const Image = ({ image }) => {
     return (
         <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
             <div className="card">
-                <img src={image.previewURL} className="card-img-top" />
+                <img
+                    src={image.previewURL}
+                    className="card-img-top"
+                    alt={image.tags}
+                />
+                <div className="card-body">
+                    <p className="card-text">{image.likes} likes</p>
+                    <p className="card-text">{image.views} views</p>
+                </div>
+                <div className="card-footer">
+                    <a
+                        href={image.largeImageURL}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="btn btn-primary btn-block"
+                    >
+                        View Image
+                    </a>
+                </div>
             </div>
         </div>
     );
