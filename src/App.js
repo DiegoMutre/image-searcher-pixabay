1f6af2a0adcbb44d42bf593add208fd49013cc71
DiegoMutre <tysonmutre10@gmail.com>
2021-02-01 06:31:52 -0500

    feat: Create queryTerm state in App component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { useEffect, useState } from "react";
 import Form from "./components/Form";
 
 function App() {
+    // State for query term
+    const [queryTerm, setQueryTerm] = useState("");
+
     return (
         <div className="container">
             <div className="jumbotron">
                 <p className="lead text-center">Image Search</p>
-                <Form />
+                <Form setQueryTerm={setQueryTerm} />
             </div>
         </div>
     );
