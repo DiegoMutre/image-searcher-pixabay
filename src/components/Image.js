e3055cd73da7e0d142492fe26bf09b7a9af55a20
DiegoMutre <tysonmutre10@gmail.com>
2021-02-04 17:47:09 -0500

    refactor: Change Image component props, doing destructuring.

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,19 +1,15 @@
-const Image = ({ image }) => {
+const Image = ({ hit: { previewURL, tags, likes, views, largeImageURL } }) => {
     return (
         <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4 animate__animated animate__fadeIn">
             <div className="card">
-                <img
-                    src={image.previewURL}
-                    className="card-img-top"
-                    alt={image.tags}
-                />
+                <img src={previewURL} className="card-img-top" alt={tags} />
                 <div className="card-body">
-                    <p className="card-text">{image.likes} likes</p>
-                    <p className="card-text">{image.views} views</p>
+                    <p className="card-text">{likes} likes</p>
+                    <p className="card-text">{views} views</p>
                 </div>
                 <div className="card-footer">
                     <a
-                        href={image.largeImageURL}
+                        href={largeImageURL}
                         target="_blank"
                         rel="noopener noreferrer"
                         className="btn btn-primary btn-block"
