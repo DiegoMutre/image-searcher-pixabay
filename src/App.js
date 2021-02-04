bdbb958dd99015e349aaf02ad252c3a0e9a150ea
DiegoMutre <tysonmutre10@gmail.com>
2021-02-04 17:36:21 -0500

    refactor: Comment the parts that will be removed after the new implementation.

--- src/App.js
+++ src/App.js
@@ -7,62 +7,53 @@ function App() {
     // State for query term
     const [queryTerm, setQueryTerm] = useState("");
 
-    // State for actual page
-    const [actualPage, setActualPage] = useState(1);
-
-    // State for total pages
-    const [totalPages, setTotalPages] = useState(5);
-
-    // State for image result
-    const [imageResult, setImageResult] = useState([]);
-
     // State for error
-    const [error, setError] = useState(false);
+    const [hasError, setHasError] = useState(false);
 
-    // Fetch the api when queryTerm changes
-    useEffect(() => {
-        if (queryTerm) {
-            const fetchResults = async _ => {
-                const apiKey = process.env.REACT_APP_API_KEY;
-                const imagesPerPage = 30;
-                const url = `https://pixabay.com/api/?key=${apiKey}&q=${queryTerm}&page=${actualPage}&per_page=${imagesPerPage}`;
+    // // Fetch the api when queryTerm changes
+    // useEffect(() => {
+    //     if (queryTerm) {
+    //         const fetchResults = async _ => {
+    //             const apiKey = process.env.REACT_APP_API_KEY;
+    //             const imagesPerPage = 30;
+    //             const url = `https://pixabay.com/api/?key=${apiKey}&q=${queryTerm}&page=${actualPage}&per_page=${imagesPerPage}`;
 
-                const res = await fetch(url);
-                const data = await res.json();
+    //             const res = await fetch(url);
+    //             const data = await res.json();
 
-                if (!data.hits.length) {
-                    setImageResult([]);
-                    setError(true);
-                    return;
-                }
+    //             if (!data.hits.length) {
+    //                 setImageResult([]);
+    //                 setHasError(true);
+    //                 return;
+    //             }
 
-                setError(false);
-                setImageResult(data.hits);
-                const calculateTotalPages = Math.ceil(
-                    data.totalHits / imagesPerPage
-                );
-                setTotalPages(calculateTotalPages);
+    //             setHasError(false);
+    //             setImageResult(data.hits);
+    //             const calculateTotalPages = Math.ceil(
+    //                 data.totalHits / imagesPerPage
+    //             );
+    //             setTotalPages(calculateTotalPages);
 
-                const $jumbotron = document.querySelector(".jumbotron");
-                $jumbotron.scrollIntoView({ behavior: "smooth" });
-            };
-            fetchResults();
-        }
-    }, [queryTerm, actualPage]);
+    //             const $jumbotron = document.querySelector(".jumbotron");
+    //             $jumbotron.scrollIntoView({ behavior: "smooth" });
+    //         };
+    //         fetchResults();
+    //     }
+    // }, [queryTerm, actualPage]);
 
     return (
         <div className="container">
             <div className="jumbotron">
                 <p className="lead text-center">Image Search</p>
                 <Form
-                    setActualPage={setActualPage}
+                    // setActualPage={setActualPage}
                     setQueryTerm={setQueryTerm}
                 />
             </div>
             <div className="row justify-content-center">
-                <ImageContainer imageResult={imageResult} error={error} />
+                {/* <ImageContainer /> */}
 
-                {actualPage > 1 && (
+                {/* {actualPage > 1 && (
                     <Button
                         action="previous"
                         actualPage={actualPage}
@@ -79,7 +70,7 @@ function App() {
                         textContent="Next >>"
                         totalPages={totalPages}
                     />
-                ) : null}
+                ) : null} */}
             </div>
         </div>
     );
